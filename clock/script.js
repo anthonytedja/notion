1327fb5f8c3fcbf75b9a3a73f78b9efe3db4560d
Anthony Tedja <anthonytedja27@gmail.com>
2022-02-12 18:10:25 -0500

    Add new clock widget

---
+++ clock/script.js
@@ -0,0 +1,37 @@
+const deg = 6;
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
+
+const setClock = () => {
+	let day = new Date();
+	let hh = day.getHours() * 30;
+	let mm = day.getMinutes() * deg;
+	let ss = day.getSeconds() * deg;
+
+	hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+	min.style.transform = `rotateZ(${mm}deg)`;
+	sec.style.transform = `rotateZ(${ss}deg)`;
+};
+
+setClock();
+setInterval(setClock, 1000);
+
+window.onclick = () => {
+	const currentTheme = localStorage.getItem("data-theme");
+	console.log(currentTheme);
+	if (currentTheme === "dark") {
+		localStorage.setItem("data-theme", "light");
+		document.documentElement.setAttribute("data-theme", "light");
+	} else {
+		localStorage.setItem("data-theme", "dark");
+		document.documentElement.setAttribute("data-theme", "dark");
+	}
+};
+
+let currentTheme = "dark";
+currentTheme = localStorage.getItem("data-theme")
+
+if (currentTheme) {
+	document.documentElement.setAttribute("data-theme", currentTheme);
+}

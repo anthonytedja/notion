45fbc105bdc43bea2b139edda49abb8b6ecc8019
Anthony Tedja <anthonytedja27@gmail.com>
2022-03-01 15:19:13 -0500

    Fix Double Click on Animate

--- kirby/script.js
+++ kirby/script.js
@@ -1,18 +1,23 @@
 const touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
-document.getElementById('image').addEventListener(touchEvent, function(e) {
+document.getElementById('image').addEventListener(touchEvent, animate);
+
+function animate() {
 	if (!e) var e = window.event;
 	e.cancelBubble = true;
 	if (e.stopPropagation) e.stopPropagation();
+
+	if (animate.isRunning) return;
+	animate.isRunning = true;
+
 	console.log('animate');
 	image = document.getElementById('image');
 	image.src = 'kirby-awake.gif';
-	document.body.style.pointerEvents = 'none';
 
 	setTimeout(() => {
 		image.src = 'kirby-asleep.gif';
-		document.body.style.pointerEvents = 'auto';
+		animate.isRunning = false;
 	}, 3675);
-});
+}
 
 var image = (new Image().src = 'kirby.gif');
 
